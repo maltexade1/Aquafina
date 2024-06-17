68a357d07c3b17673a65037bc5889c012f3ea31d
Godstime315 <97334195+Godstime315@users.noreply.github.com>
2024-06-17 08:40:36 +0100

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -91,8 +91,7 @@ class Database:
         return user.get('ban_status', default)
 
     async def get_all_users(self):
-        users_list = (await (self.col.find({})).to_list(length=None))+(await (self.col2.find({})).to_list(length=None))
-        return users_list
+        return self.col.find({})
     
 
     async def delete_user(self, user_id):
