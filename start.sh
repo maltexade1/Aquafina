eb6ef0cde79ca79c66c2bd3788673c1257ba4eb6
Godstime315 <97334195+Godstime315@users.noreply.github.com>
2024-06-19 10:59:42 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Hislordshipb/Hislordshipb.git /DQ
+  git clone https://github.com/Hislordshipb/Hislordshipb.git /Hislordshipb
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Hislordshipb
